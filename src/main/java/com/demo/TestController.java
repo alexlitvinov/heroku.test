2526079fb33b2219b11a5d22fb9d6fb48ea953ad
Alexandr Litvinov. Software Developer <Aleksandr.Litvinov.01@privatbank.ua>
2016-06-23 12:06:43 +0300

    webhook

--- src/main/java/com/demo/TestController.java
+++ src/main/java/com/demo/TestController.java
@@ -5,20 +5,32 @@
  */
 package com.demo;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
+    private final String MY_TOKEN = "430321c0901d0ee6a0eb3541a9b5d3c6";
+
     @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World1") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World1") String name, Model model) {
         model.addAttribute("name", name);
-        return  "greeting";
+        return "greeting";
     }
-    
-    
+
+    @RequestMapping("/webhook")
+    @ResponseBody
+    public String greeting(HttpServletRequest req, HttpServletResponse res) {
+        if (req.getParameter("hub.verify_token").equals(MY_TOKEN)) {
+            return req.getParameter("hub.challenge");
+        } else {
+            return "Error, wrong validation token";
+        }
+    }
 }
