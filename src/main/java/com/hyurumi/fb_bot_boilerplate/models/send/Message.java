8ec85c28285c680174e330e09e90701e72187b95
QuiwiDeveloper <QuiwiDeveloper@192.168.0.105>
2016-06-27 23:10:35 +0300

    сграбил структуры отсюда https://github.com/hyurumi/fb-bot-boilarplate-java.git

--- src/main/java/com/hyurumi/fb_bot_boilerplate/models/send/Message.java
+++ src/main/java/com/hyurumi/fb_bot_boilerplate/models/send/Message.java
@@ -16,16 +16,10 @@ import okhttp3.MediaType;
  * Created by genki.furumi on 4/15/16.
  */
 public class Message {
-    public static final MediaType JSON= MediaType.parse("application/json; charset=utf-8");
     private String END_POINT = "https://graph.facebook.com/v2.6/me/messages";
-    private transient final OkHttpClient HTTP_CLIENT;
-    private transient final Gson GSON;
         private final String PAGE_TOKEN="EAAEUQLpUz1YBANLyFeWjimqr9IOXxZAdx0fFzaTEQLve8dxZAdZAyOoAsX9f1iaHAPeBdnuBmTJdgBiC1RVCm9vK6MCFYwiq91lQQJZASnH2NT8TNEH6RZCQffLZAK7b6aMBnQxGXICFBAmmZCtQwgSXZBt3UvoXtxMZBM1DeBkTDm2UZAjZBkvZCbQA";
     
-    {
-        HTTP_CLIENT = new OkHttpClient();
-        GSON = new Gson();
-    }
+
 
     final private Attachment attachment;
     final private String text;
@@ -59,31 +53,19 @@ public class Message {
         return attachment.addButton(button);
     }
 
-    public Response sendTo(String recipientId) throws IOException {
-
-        RequestBody body = RequestBody.create(JSON, GSON.toJson(new MessageWrapper(recipientId, this)));
-        System.out.println("try send "+GSON.toJson(new MessageWrapper(recipientId, this)));
-        Request request = new Request.Builder()
-                .url(END_POINT + "?access_token=" + PAGE_TOKEN)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .post(body)
-                .build();
-        okhttp3.Response response = HTTP_CLIENT.newCall(request).execute();
-        final int code = response.code();
-        Response returnValue = null;
-        if (code == 200) {
-            System.out.println("WORK!");
-            returnValue = GSON.fromJson(response.body().string(), Response.class);
-        }else {
-            System.out.println("ERROR: " + response.body().string());
-        }
-        response.body().close();
-        return returnValue;
-    }
-
     public static class MessageWrapper {
         private final Recipient recipient;
         private final Message message;
+
+        public Recipient getRecipient() {
+            return recipient;
+        }
+
+        public Message getMessage() {
+            return message;
+        }
+        
+        
         public MessageWrapper(String recipientId, Message message) {
             this.recipient = new Recipient(recipientId);
             this.message = message;
