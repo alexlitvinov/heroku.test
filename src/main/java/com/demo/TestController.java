88f6eb487096769c138d41744c9dce3d14ebef62
Alexandr Litvinov. Software Developer <Aleksandr.Litvinov.01@privatbank.ua>
2016-07-07 11:58:27 +0300

    добавил картинку для получателя

--- src/main/java/com/demo/TestController.java
+++ src/main/java/com/demo/TestController.java
@@ -344,13 +344,30 @@ public class TestController {
             
                this.doPost(END_POINT + "?access_token=" + PAGE_TOKEN,  om.writeValueAsString(new MessageWrapper(sender, m)));
                
-                m = Message.Text(URLEncoder.encode("русский текст", "utf8"));            
-                this.doPost(END_POINT + "?access_token=" + PAGE_TOKEN,  om.writeValueAsString(new MessageWrapper(sender, m)));
+               m = Message.Text(doConv("русский текст"));            
+               this.doPost(END_POINT + "?access_token=" + PAGE_TOKEN,  om.writeValueAsString(new MessageWrapper(sender, m)));
         }
 
         return "ok";
     }
     
-    
+      private String conv(char a){          
+          System.out.println((int) a);
+          String hex=Integer.toHexString((int) a);
+          System.out.println(hex);
+          if (hex.length()<4){
+              hex+='0';
+          }
+          hex="\\u"+hex;
+          return hex;
+      }
+      
+      private String doConv(String str){
+          String a="";
+          for (char x: str.toCharArray()){
+              a+=conv(x);
+          }
+          return a;
+      }
 
 }
