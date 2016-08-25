09cdc4a9425ad843a83839e34c9f07989f018717
Alexandr Litvinov. Software Developer <Aleksandr.Litvinov.01@privatbank.ua>
2016-08-25 13:30:27 +0300

    исправил ошибку при выборе  связей клиента

--- src/main/resources/static/registration.js
+++ src/main/resources/static/registration.js
@@ -1,66 +1,102 @@
-var phoneError='';
-var successSt="success";
-var incorrect="is-invalid";
+var phoneError = '';
+var successSt = "success";
+var incorrect = "is-invalid";
 
-function checkValid(elem){
+function checkValid(elem) {
     return !$(elem).hasClass(incorrect);
 }
 
-function showloader(){
+function showloader() {
     $('#loader').css({display: 'block'});
 }
 
-function hideloader(){
+function hideloader() {
     $('#loader').css({display: 'none'});
 }
 
-function checkPhone(){
+function checkPhone() {
     showloader();
     $.ajax({
         dataType: "json",
-        url: 'phoneCheck?phone='+$('#phoneInput').val(),
+        url: 'phoneCheck?phone=' + $('#phoneInput').val(),
         data: {},
-        success: function(data){
+        success: function (data) {
             hideloader();
-            if (data.status!==successSt){                
-                $('#phoneDiv').addClass(incorrect);                
+            if (data.status !== successSt) {
+                $('#phoneDiv').addClass(incorrect);
             } else {
                 $('#getcards-step').addClass('is-visible');
+                $('#phone-step').css({display:'none'});
+                $('#step-counter').html('Step (2/3)');
             }
         }
     });
 }
 
-function checkCard(){
+function checkCard() {
     showloader();
     $.ajax({
         dataType: "json",
-        url: 'cardCheck?card='+$('#cardInput').val(),
+        url: 'cardCheck?card=' + $('#cardInput').val(),
         data: {},
-        success: function(data){
+        success: function (data) {
             hideloader();
-            if (data.status!==successSt){                
-                $('#cardDiv').addClass(incorrect);                
+            if (data.status !== successSt) {
+                $('#cardDiv').addClass(incorrect);
             } else {
+                $('#getcards-step').css({display:'none'});
                 $('#getotp-step').addClass('is-visible');
+                $('#step-counter').html('Step (3/3)');
             }
         }
     });
 }
 
-function checkOtp(){
+function checkOtp() {
     showloader();
     $.ajax({
         dataType: "json",
-        url: 'otpCheck?otp='+$('#otpInput').val()+'&token='+$('#token').val(),
+        url: 'otpCheck?otp=' + $('#otpInput').val() + '&token=' + $('#token').val(),
         data: {},
-        success: function(data){
+        success: function (data) {
             hideloader();
-            if (data.status!==successSt){                
-                $('#otpDiv').addClass(incorrect);                
+            if (data.status !== successSt) {
+                $('#otpDiv').addClass(incorrect);
             } else {
+                $('#getotp-step').css({display:'none'});
                 $('#end-step').addClass('is-visible');
             }
         }
     });
 }
+
+$(document).ready(function () {
+    $('#otpInput').on("keypress", function (e) {
+        /* ENTER PRESSED*/
+        if (e.keyCode === 13) {
+            checkOtp();
+            return false;
+        }
+    });
+    $('#cardInput').on("keypress", function (e) {
+        /* ENTER PRESSED*/
+        if (e.keyCode === 13) {
+            checkOtp();
+            return false;
+        }
+    });
+    $('#cardInput').on("keypress", function (e) {
+        /* ENTER PRESSED*/
+        if (e.keyCode === 13) {
+            checkCard();
+            return false;
+        }
+    });
+    $('#phoneInput').on("keypress", function (e) {
+        /* ENTER PRESSED*/
+        if (e.keyCode === 13) {
+            checkPhone();
+            return false;
+        }
+    });
+});
